43b8d9815899ec6ac3b5beac9ee1cf19f6ec525e
nishithakur04 <129810284+nishithakur04@users.noreply.github.com>
2023-11-07 16:08:30 +0530

    Add files via upload

---
+++ Grocery_App/App.js
@@ -0,0 +1,28 @@
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductPage from "./pages/ProductPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <HomePage /> } />
+                <Route path="/products/:catId" element={ <ProductPage /> } />
+                <Route path="/products/detail/:id" element={ <ProductDetailPage /> } />
+                <Route path="/about" element={ <AboutPage /> } />
+                <Route path="/contact" element={ <ContactPage /> } />
+                <Route path="/register" element={ <RegisterPage /> } />
+                <Route path="/login" element={ <LoginPage /> } />
+                <Route path="*" element={ <ErrorPage /> } />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
